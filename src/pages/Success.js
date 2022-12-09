59ec5dfb726549b8bc0ec6387080b3ce1e6a1408
Lai Ze Min <zeminlai426@gmail.com>
2022-12-09 17:07:58 +0800

    success button redirect to homepage

--- src/pages/Success.js
+++ src/pages/Success.js
@@ -1,11 +1,14 @@
 import {Button} from 'react-bootstrap'
 const Success = () => {
+    const redirectHome = () => {
+        window.location.href= "http://localhost:8080/home"
+    }
     return (  
         <>
             <h1 align="center">Payment Successful</h1>
             <h3 align="center">Thank you for purchasing!</h3>
 
-            <Button variant='success' className='homepage-btn btn btn-primary mx-auto d-block m-5'><a href="/home">Back to home page</a></Button>        
+            <Button onClick={redirectHome} variant='success' className='homepage-btn btn btn-primary mx-auto d-block m-5'>Back to home page</Button>        
 
 
         </>
